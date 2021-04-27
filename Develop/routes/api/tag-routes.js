da89a287eb2ea95e04201c0d8bb19b71379213e9
Janeé Mays <janee.elise@gmail.com>
2021-04-27 09:59:17 -0400

    changed id on line 23 of tag-routes in attempt to fix bug

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -20,12 +20,11 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(
-      id: req.params.id, {
+      req.params.id, {
       include: [
         {
           model: Product,
           through: ProductTag,
-          as: 'tag_product'
         }]
     });
     res.status(200).json(tagData);
